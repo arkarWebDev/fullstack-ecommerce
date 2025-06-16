7f838b43e18a7dde550d0c00ad8e21c7788ea65b
arkarwebdev <arkar84848484@gmail.com>
2025-06-16 09:45:51 -0700

    client base template

---
+++ client/src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Main from "./layouts/Main.tsx";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
